e02a615a7150f163e4125046792cf046013c0564
Martin Wilke <miwi@FreeBSD.org>
2011-11-09 03:57:37 +0000

    Fix build.
    Reported-by: pavbot

--- src/netstat.c
+++ src/netstat.c
@@ -243,7 +243,6 @@ PyFB_ifstats(PyObject *self)
 		Py_DECREF(t);
 		p_int(type);        p_int(physical);
 		p_int(addrlen);     p_int(hdrlen);
-		p_int(recvquota);   p_int(xmitquota);
 		p_long(mtu);        p_long(metric);
 		p_long(baudrate);   p_long(ipackets);
 		p_long(ierrors);    p_long(opackets);
